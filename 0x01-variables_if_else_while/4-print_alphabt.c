2dca2bb063e69db952328bf794afd32f2a753773
lusandaluhlabo <lusandaluhlabo@gmail.com>
2022-12-09 11:05:01 -0800

    prints alphabets in lowercase

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,13 +10,10 @@ int main(void)
 
 	while (c <= 'z')
 	{
-		if (c == 'q' || c == 'e')
-		{
-			continue;
-		}
-		putchar(c);
+		if (c != 'q' && c != 'e')
+			putchar(c);
+		c++;
 	}
-
 	putchar('\n');
 	return (0);
 }
