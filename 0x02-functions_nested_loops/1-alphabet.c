378e8f48fe6e15dc5e26557e53cf0987070791f9
lusandaluhlabo <lusandaluhlabo@gmail.com>
2022-12-13 11:57:45 -0800

    Solution for task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main -  print the alphabet in lowercase followed by a new line
+ * print_alphabet -  print the alphabet in lowercase followed by a new line
  *
  * Return: 0
  */
