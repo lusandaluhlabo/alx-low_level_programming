e755fa9213819a72514954aecd340cb5a7070639
lusandaluhlabo <lusandaluhlabo@gmail.com>
2022-12-14 11:25:07 -0800

    Solution for task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - add two integers and returns the result
+ * @x: int type number
+ * @y: int type number
+ * Return: result of addition
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
