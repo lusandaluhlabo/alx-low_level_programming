7b39cb4fefde4292360aba7ebaf5dc561d446b01
lusandaluhlabo <lusandaluhlabo@gmail.com>
2022-12-15 13:44:35 -0800

    prints number

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
  * print_numbers - print the numbers from 0 to 9
- * Description: prints the numbers, from 0 to 9
+ * Description: prints the numbers from 0 to 9
  */
 void print_numbers(void)
 {
-	int = i;
+	int i;
 
 	i = 0;
 	while (i < 10)
