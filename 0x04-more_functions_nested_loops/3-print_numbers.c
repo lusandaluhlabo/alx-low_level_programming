09ee44cf927e739b812833eff9d6798ab518bced
lusandaluhlabo <lusandaluhlabo@gmail.com>
2022-12-15 13:48:10 -0800

    Prints numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,8 @@
 #include "main.h"
+
 /**
  * print_numbers - print the numbers from 0 to 9
- * Description: prints the numbers from 0 to 9
+ * Description: You can only use _putchar twice
  */
 void print_numbers(void)
 {
