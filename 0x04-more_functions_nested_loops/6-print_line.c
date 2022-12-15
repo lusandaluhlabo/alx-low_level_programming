d747af7536be1ead225487c6675843fc70b36b21
lusandaluhlabo <lusandaluhlabo@gmail.com>
2022-12-15 15:52:57 -0800

    print 10 times

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - draw a straight line in the terminal
+ *
+ * Description:
+ * Where n is the number of times the character _ should be printed
+ * The line should end with a \n
+ * If n is 0 or less, the function should only print \n
+ *
+ * Can only use _putchar function to print
+ */
+void print_line(int n)
+{
+	int c, i;
+
+	c = 0;
