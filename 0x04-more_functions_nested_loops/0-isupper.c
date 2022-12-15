2b21622db33311e963e006f229eee328b6e951e2
lusandaluhlabo <lusandaluhlabo@gmail.com>
2022-12-15 11:52:18 -0800

    Checks uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,13 +3,12 @@
 /**
  * _isupper - check for uppercase character
  * @c: int type letter
- *
- * Return: 1 if uppercase, else 0
+ * Return: 1 if uppercase, 0 if not uppercase
  */
 int _isupper(int c)
 {
 	if (c >= 65 || c <= 90)
 		return (1);
-
-	return (0);
+	else
+		return (0);
 }
