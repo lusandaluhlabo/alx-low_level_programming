734105cee5b454b9f516e21a78cc1c0dc717a457
lusandaluhlabo <lusandaluhlabo@gmail.com>
2022-12-16 05:02:36 -0800

    Fizz-Buzz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: multiples of three print Fizz
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf('\n');
+
+	return (0);
+}
