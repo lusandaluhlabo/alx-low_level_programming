db2dc3fd31c3ec24717a862389eedfaec5929cb8
lusandaluhlabo <lusandaluhlabo@gmail.com>
2022-12-16 05:30:41 -0800

    Fizz-Buzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,26 +9,17 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	i = 1;
+	while (i <= 100)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-		{
 			printf("FizzBuzz");
-		}
-
 		else if (i % 3 == 0)
-		{
 			printf("Fizz");
-		}
-
 		else if (i % 5 == 0)
-		{
 			printf("Buzz");
-		}
 		else
-		{
 			printf("%d ", i);
-		}
 	}
 	printf('\n');
 
