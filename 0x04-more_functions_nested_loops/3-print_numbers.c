7b55b5173377987c35e70262f4288d4abb5f0648
lusandaluhlabo <lusandaluhlabo@gmail.com>
2022-12-15 16:20:11 -0800

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,11 +7,10 @@
  */
 void print_numbers(void)
 {
-	int i;
-
-	for (int i = 0; i < 10; i++)
+	char c;
+	for (c = '0'; c <= '0'; c++)
 	{
-		_putchar(i + '0');
+		_putchar(c);
 	}
 	_putchar('\n');
 }
