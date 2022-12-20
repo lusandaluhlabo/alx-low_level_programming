f7d002a29f4680eaf6e173a1e6337f64a2bb1783
lusandaluhlabo <lusandaluhlabo@gmail.com>
2022-12-20 13:49:18 -0800

    solution for task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverse a string
+ * @s: string to reverse
+ * Return: nothing
+ */
+void rev_string(char *s)
+{
+	int = 0, lenght;
+
+	lenght = _strlen(s) - 1;
+
+	while (lenght > i)
+	{
+		swap_char(s + lenght, s + i);
+		i++;
+		leght--;
+	}
+}
+
+/**
+ * _strlen - returns the lenght of a string
+ * @s: string
+ * Return: returns lenght;
+ */
+int _strlen(char *s)
+{
+	int count, inc;
+
+	inc = 0;
+
+	for (count = 0; s[count] != '\0'; count++)
+		inc++;
+
+	return (inc);
+}
+
+/**
+ * swap_char - swap two character
+ * @a: first integer
+ * @b: second integer
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+	char tmp = *a;
+
+	*a = *b;
+	*b = tmp;
+}
