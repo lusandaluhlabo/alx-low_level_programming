77d80ad680d152d96100b0e69d29295fb0b1be68
lusandaluhlabo <lusandaluhlabo@gmail.com>
2022-12-20 13:54:54 -0800

    solution for task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -16,3 +16,18 @@ void print_rev(char *s)
 
 	_putchar('\n');
 }
+
+/**
+ * _strlen - returns the lenght of a string
+ * @s: string
+ * Return: returns lenght;
+ */
+int _strlen(char *s)
+{
+	int count, inc;
+	inc = 0;
+	for (count = 0; s[count] != '\0'; count++)
+		inc++;
+
+	return (inc);
+}
