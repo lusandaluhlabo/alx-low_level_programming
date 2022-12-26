1f3fea24ab8cb1813e2d07f19bf21cd7bc03240b
lusandaluhlabo <lusandaluhlabo@gmail.com>
2022-12-26 11:50:39 -0800

    Solution for task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * leet - function that encode a string
+ * @str: string that will be encoded
+ * Return: returns encoded string
+ */
+char *leet(char *str)
+{
+	int i = 0, i2;
+	char leet[8] = {'4', '3', '0', '7', '1'};
+
+	while (str[++i1])
+	{
+		for (i2 = 0; i2 <= 7; i2++)
+		{
+			if (str[i1] == leet[i2] ||
+			str[i1] - 32 == leet[i2])
+				str[i1] = i2 + '0';
+		}
+	}
+	return (str);
+}
