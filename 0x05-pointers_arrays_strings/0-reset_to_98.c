ac5f584a9564e45ee48b3ab22181f92c9eb231c3
lusandaluhlabo <lusandaluhlabo@gmail.com>
2022-12-30 00:33:12 -0800

    Solution for task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * reset_to_98 -  updates the value it points to to 98
+ * reset_to_98 - updates the value it points to to 98
  * @n: parameter targeted
+ *
  * Return: nothing
  */
 void reset_to_98(int *n)
