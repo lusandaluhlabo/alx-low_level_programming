328b71c05d0771cb6c49d5d4f4f723b331e65804
lusandaluhlabo <lusandaluhlabo@gmail.com>
2023-01-03 12:54:46 -0800

    Solution for task 6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,14 +7,12 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	for (int i = 0; i < 8; i++)
-	{
-		for (int j = 0; j < 8; j++)
-		{
-			putchar(a[i][j]);
-			putchar(' ');
-		}
+	int i, j;
 
+	for (i = 0; a[i][7]; i++)
+	{
+		for (j = 0; j < 8; j++)
+			putchar(a[i][j]);
 		putchar('\n');
 	}
 }
