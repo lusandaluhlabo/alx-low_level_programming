049b65b8773eaf685edf762c445baacd4044bd39
lusandaluhlabo <lusandaluhlabo@gmail.com>
2023-01-05 13:32:28 -0800

    Solution for task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,33 +2,29 @@
 
 /**
  * _sqrt_recursion - returns the natural square root of a number
- * halp - helper function to solve _sqrt_recursion
+ * halp - helper function to sn
  * @n: int number
  * Return: natural square root
  */
-
 int _sqrt_recursion(int n)
 {
-	return (halp(n, 1));
+	if (n == 0 || n == 1)
+		return (n);
+	return (_sqrt(0, n));
 }
 
 /**
- * halp - helper function to solve _sqrt_recursion
- * @c: number to determine if square root
- * @i: incrementer to compare against 'c'
- * Return: square root if natural square root, or -1 if none found
+ * _sqrt - returns the square root of a number
+ * @n: test number
+ * @x: squared number
+ * Return: square root of n
  */
 
-int halp(int c, int i)
+int _sqrt(int n, int x)
 {
-	int square;
-
-	square = i * i;
-
-	if (square == c)
-		return (i);
-	else if (square < c)
-		return (halp(c, i + 1));
-	else
+	if (n > x / 2)
 		return (-1);
+	else if (n * n == x)
+		return (n);
+	return (_sqrt(n + 1, x));
 }
