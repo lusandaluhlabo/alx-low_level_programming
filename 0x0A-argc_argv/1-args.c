92283872f0b0b5c2f2f10f48a0dca26f47d76a7b
lusandaluhlabo <lusandaluhlabo@gmail.com>
2023-01-09 12:57:53 -0800

    Solution for task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints number of arguments
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: number of arguments
+ */
+
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
