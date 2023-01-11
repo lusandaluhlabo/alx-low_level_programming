4ebab3373b2623ffbad17a7bbac03ab61fc037ae
lusandaluhlabo <lusandaluhlabo@gmail.com>
2023-01-11 13:29:10 -0800

    Solution for task 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -51,7 +51,7 @@ int check_palindrome(char *s, int len, int index)
 		return (1);
 
 	if (s[index] == s[len - index - 1])
-		return (check_palindrome(s. len, index + 1));
+		return (check_palindrome(s, len, index + 1));
 
 	return (0);
 }
