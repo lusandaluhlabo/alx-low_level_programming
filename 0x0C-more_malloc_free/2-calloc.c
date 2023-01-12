93bf5f4058e63239be8b65dde021f80c96784999
lusandaluhlabo <lusandaluhlabo@gmail.com>
2023-01-12 13:17:51 -0800

    Solution for task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,16 +11,16 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	if (nmeb == 0 || size == 0)
+	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
-	void *ptr = malloc(nmeb * size);
+
+	void *ptr = malloc(nmemb * size);
 
 	if (ptr == NULL)
-	{
 		return (NULL);
-	}
+
 	memset(ptr, 0, nmemb * size);
 	return (ptr);
 }
