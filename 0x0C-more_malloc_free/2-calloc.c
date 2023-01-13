75f6176f21553ef77bb49d2f9de588a43ecad153
lusandaluhlabo <lusandaluhlabo@gmail.com>
2023-01-13 12:24:59 -0800

    Solution for task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,12 +13,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *block;
 	unsigned int i;
 
-	if (nmemb == 0 || size 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 	block = malloc(nmemb * size);
 	if (block != NULL)
 	{
-		for (i = 0, i < (nmemb * size); i++)
+		for (i = 0; i < (nmemb * size); i++)
 			block[i] = 0;
 		return (block);
 	}
